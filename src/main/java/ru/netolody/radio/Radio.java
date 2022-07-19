7eb0aff175c924d2698182604cbb06ac1b0d5baa
DashaSlesareva <zalezka@yandex.ru>
2022-07-19 20:49:41 +0300

    Second commit

--- src/main/java/ru/netolody/radio/Radio.java
+++ src/main/java/ru/netolody/radio/Radio.java
@@ -14,7 +14,54 @@ public class Radio {
         this.channel = channel;
     }
 
-    public int getChannel () {
+    public int getChannel() {
         return channel;
     }
+
+    public void nextChannel() {
+        if (this.channel == 9) {
+            this.channel = 0;
+        } else {
+            this.channel++;
+        }
+    }
+
+    public void prevChannel() {
+        if (this.channel == 0) {
+            this.channel = 9;
+        } else {
+            this.channel--;
+        }
+    }
+
+
+    private int volume;
+
+    public void setVolume(int volume) {
+        if (volume < 0) {
+            return;
+        }
+        if (volume > 10) {
+            return;
+        }
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void increaseVolume() {
+        if (volume < 10) {
+            volume = volume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (volume > 0) {
+            volume = volume - 1;
+        }
+    }
+
+
 }
--- src/test/java/ru/netolody/radio/RadioTest.java
+++ src/test/java/ru/netolody/radio/RadioTest.java
@@ -2,17 +2,67 @@ package ru.netolody.radio;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RadioTest {
 
-    @Test
-    void setChannelTest() {
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/setChannelTestData.csv"})
+    void setChannelTest(int channel, int expected) {
         Radio radio = new Radio();
-        int expected = 3;
-        radio.setChannel(3);
+        radio.setChannel(channel);
         int actual = radio.getChannel();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/prevChannelTestData.csv"})
+    void prevChannelTest(int channel, int expected) {
+        Radio radio = new Radio();
+        radio.setChannel(channel);
+        radio.prevChannel();
+        int actual = radio.getChannel();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/nextChannelTestData.csv"})
+    void nextChannelTest(int channel, int expected) {
+        Radio radio = new Radio();
+        radio.setChannel(channel);
+        radio.nextChannel();
+        int actual = radio.getChannel();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/setVolumeTestData.csv"})
+    void setVolumeTest(int volume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolume(volume);
+        int actual = radio.getVolume();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/increaseVolumeTestData.csv"})
+    void increaseVolumeTest(int volume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolume(volume);
+        radio.increaseVolume();
+        int actual = radio.getVolume();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/decreaseVolumeTestData.csv"})
+    void decreaseVolumeTest(int volume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolume(volume);
+        radio.decreaseVolume();
+        int actual = radio.getVolume();
         assertEquals(expected, actual);
     }
 
